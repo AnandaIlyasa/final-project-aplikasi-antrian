e8a8f1f07b4562aef68161185c6aa0f9a7b5dda3
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2021-04-19 22:35:37 +0800

    bug nav sudah done

--- app/src/main/java/com/finalproject/queue/userFragment/HomeFragment.kt
+++ app/src/main/java/com/finalproject/queue/userFragment/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.finalproject.queue.userFragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.Navigation
 import com.finalproject.queue.R
 import com.finalproject.queue.databinding.FragmentHomeBinding
@@ -20,6 +22,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+@Suppress("DEPRECATION")
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private lateinit var binding: FragmentHomeBinding
