0b11296b6d9d6a4fe4831ef79c493771d030acd2
AnandaIlyasa <anandailyasa23244@student.uns.ac.id>
2021-04-20 22:30:27 +0800

    ganti warna background and firebase dependency

--- app/src/main/java/com/finalproject/queue/userFragment/LoginFragment.kt
+++ app/src/main/java/com/finalproject/queue/userFragment/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.finalproject.queue.userFragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,8 +10,16 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.Navigation
+import com.finalproject.queue.MainActivity
 import com.finalproject.queue.R
 import com.finalproject.queue.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
 // TODO: Rename parameter arguments, choose names that match
